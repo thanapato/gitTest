d685c90e0635ab50fd4b4a2a2896fa170e988ce2
thanapat <thanapat.tle@mail.kmutt.ac.th>
2020-10-20 15:32:13 +0700

    First Commit

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class GitTest {
+
+  
+    public static void main(String[] args) {
+       sayHello();
+    }
+    
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    
+}
